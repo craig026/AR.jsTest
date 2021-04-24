a62393bff8fcc7eb1b6533ebe563a3955841ebe9
craig026 <54131430+craig026@users.noreply.github.com>
2021-04-24 10:20:36 +0800

    Add button when object visible code

--- js/simpleevents.js
+++ js/simpleevents.js
@@ -2,9 +2,27 @@ AFRAME.registerComponent('accepts-clicks', {
     init: function () {
         this.el.addEventListener('touchend', handleClickEvent);
         this.el.addEventListener('click', handleClickEvent);
+        this.el.addEventListener('markerFound', showText);
     }
 });
 
 function handleClickEvent() {
     window.location = 'https://sencom.com.au/';
-}
+};
+
+function showText() {
+    var marker = document.querySelector("pattern-logo");
+    if (marker && marker.object3D.visible) {
+        setUpButton();
+    }
+};
+
+function setUpButton() {
+    var button = document.querySelector(".button");
+    if (button.style.display === 'none' || !button.style.display) {
+        button.innerHTML = "This is a model!";
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+};
