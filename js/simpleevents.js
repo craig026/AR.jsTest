ef53f76f5fefe1b2ed3c9cd460bc5fd588ba9c6e
craig026 <54131430+craig026@users.noreply.github.com>
2021-04-25 20:36:49 +0800

    Update simpleevents.js

--- js/simpleevents.js
+++ js/simpleevents.js
@@ -2,19 +2,16 @@ AFRAME.registerComponent('accepts-clicks', {
     init: function () {
         this.el.addEventListener('touchend', handleClickEvent);
         this.el.addEventListener('click', handleClickEvent);
-        //this.el.addEventListener('markerFound', showText);
+
+		var m = document.querySelector("a-marker")
+		    m.addEventListener("markerFound", (e)=>{
+		var text = document.querySelector("text");
+			text.innerHTML = "This is a model!";
+			text.style.display = "block";
+		})
     }
 });
 
 /*function handleClickEvent() {
     window.location = 'https://sencom.com.au/';
-};*/
-
-/*function showText() {
-    var marker = document.querySelector("pattern-logo");
-    var text = document.querySelector("text");
-    if (marker.object3D.visible == true) {
-        text.innerHTML = "This is a model!";
-        text.style.display = "block";
-    }
 };*/
