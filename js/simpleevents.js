e9ca4ffa294c13e25ceed48fd7fdda3f515104c8
craig026 <54131430+craig026@users.noreply.github.com>
2021-04-25 21:16:27 +0800

    Update SentientTest

--- js/simpleevents.js
+++ js/simpleevents.js
@@ -3,12 +3,12 @@ AFRAME.registerComponent('accepts-clicks', {
         this.el.addEventListener('touchend', handleClickEvent);
         this.el.addEventListener('click', handleClickEvent);
 
-		var m = document.querySelector("a-marker")
+		/*var m = document.querySelector("a-marker")
 		    m.addEventListener("markerFound", (e)=>{
 		var text = document.querySelector("text");
 			text.innerHTML = "This is a model!";
 			text.style.display = "block";
-		})
+		})*/
     }
 });
 
