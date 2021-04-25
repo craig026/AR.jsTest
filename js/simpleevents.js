5b4cdb3aef5e52d7d79f7010bc31fe5ad4726beb
craig026 <54131430+craig026@users.noreply.github.com>
2021-04-25 23:21:59 +0800

    Add button back to SentientTest, try add function to simpleevents.js

--- js/simpleevents.js
+++ js/simpleevents.js
@@ -13,29 +13,10 @@ AFRAME.registerComponent('accepts-clicks', {
 });
 
 function showButtonEvent() {
-    <style>
-        .buttons {
-            position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        height: 5em;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        z-index: 10;
-      }
-  
-    .say-hi-button {
-            padding: 0.25em;
-        border-radius: 4px;
-        border: none;
-        background: white;
-        color: black;
-        width: 15em;
-        height: 3em;
-      }
-    </style>
+    var m = document.querySelector(".say-hi-button");
+    m.addEventListener("click", function () {
+        window.location = 'https://sencom.com.au/';
+    })
 }
 
 function handleClickEvent() {
