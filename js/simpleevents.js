1c95edc727ec100ee03b024064e3d6e648b16e6a
craig026 <54131430+craig026@users.noreply.github.com>
2021-04-25 23:12:09 +0800

    Try moving button to simpleevents.js

--- js/simpleevents.js
+++ js/simpleevents.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent('accepts-clicks', {
     init: function () {
         //this.el.addEventListener('touchend', handleClickEvent);
         //this.el.addEventListener('click', handleClickEvent);
-
+        this.el.addEventListener('showButton', showButtonEvent);
 		/*var m = document.querySelector("a-marker")
 		    m.addEventListener("markerFound", (e)=>{
 		var text = document.querySelector("text");
@@ -11,6 +11,32 @@ AFRAME.registerComponent('accepts-clicks', {
 		})*/
     }
 });
+
+function showButtonEvent() {
+    <style>
+        .buttons {
+            position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 5em;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        z-index: 10;
+      }
+  
+    .say-hi-button {
+            padding: 0.25em;
+        border-radius: 4px;
+        border: none;
+        background: white;
+        color: black;
+        width: 15em;
+        height: 3em;
+      }
+    </style>
+}
 
 function handleClickEvent() {
     window.location = 'https://sencom.com.au/';
