dd6533b71948c035e0460ab00741f535d33fada7
Craig <craig026@outlook.com>
2021-05-22 20:23:13 +0800

    Update handleclickevents.js

--- js/handleclickevents.js
+++ js/handleclickevents.js
@@ -7,11 +7,13 @@ AFRAME.registerComponent('markerhandler', {
         // every click, we make our model grow in size :)
         animatedMarker.addEventListener('click', function (ev, target) {
             const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntity && intersectedElement === aEntity && animatedMarker.object3D.visible) {
-                window.location = 'https://sencom.com.au/';
-                //const scale = aEntity.getAttribute('scale');
-                //Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-                //aEntity.setAttribute('scale', scale);
+            if (animatedMarker.object3D.visible) {
+                if (aEntity && intersectedElement === aEntity) {
+                    window.location = 'https://sencom.com.au/';
+                    //const scale = aEntity.getAttribute('scale');
+                    //Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+                    //aEntity.setAttribute('scale', scale);
+                }
             }
         });
     }
