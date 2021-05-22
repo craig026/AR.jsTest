ab9c21d1b0f070a571f328076a83be926b52728b
Craig <craig026@outlook.com>
2021-05-22 22:37:20 +0800

    Update markerevents.js

--- js/markerevents.js
+++ js/markerevents.js
@@ -3,10 +3,10 @@ AFRAME.registerComponent('markerhandler', {
         var marker = document.querySelector("#animated-marker")
 
         marker.addEventListener("markerFound", (e) => {
-            if (document.querySelector(".sentientlogo").style.display === "none") {
+            console.log(document.querySelector(".sentientlogo").style.opacity)
                 document.querySelector(".show-details-button").style.display = "block"
                 console.log("found")
-            }
+
         })
 
         marker.addEventListener("markerLost", (e) => {
